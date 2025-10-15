6fa4e654daeea3d112b85871226c8a6df2b6473e
bernes1 <37117219+bernes1@users.noreply.github.com>
2025-10-15 23:32:08 +0200

    Removed a old host and homemanager

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Darwin configuration";
-
+  
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/master";
     darwin.url = "github:lnl7/nix-darwin";
@@ -10,46 +10,11 @@
   };
 
   outputs = { self, nixpkgs, home-manager, darwin, ... }: {
-    darwinConfigurations."sovepose" = darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      specialArgs = { inherit self; };
-      modules = [
-        ./hosts/sovepose/configuration.nix
-
-        home-manager.darwinModules.home-manager
-        {
-          users.users.duck.home = "/Users/duck";
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.duck = import ./home.nix;
-
-          # Optionally, use home-manager.extraSpecialArgs to pass
-          # arguments to home.nix
-          home-manager.extraSpecialArgs = {
-            hostname = "sovepose"; 
-          };
-        }
-      ];
-    };
     darwinConfigurations."quack" = darwin.lib.darwinSystem {
       system = "aarch64-darwin";
       specialArgs = { inherit self; };
       modules = [
         ./hosts/quack/configuration.nix
-
-        home-manager.darwinModules.home-manager
-        {
-          users.users.duck.home = "/Users/duck";
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.duck = import ./home.nix;
-
-          # Optionally, use home-manager.extraSpecialArgs to pass
-          # arguments to home.nix
-          home-manager.extraSpecialArgs = {
-            hostname = "quack"; 
-          };
-        }
       ];
     };
   };
